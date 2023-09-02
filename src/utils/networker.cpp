79d6b7262c94200f76723c180c805c7954d861ea
Victor Zhou <victor.zhoujiang@tuni.fi>
2023-09-02 17:51:07 +0300

    finished networker

--- src/utils/networker.cpp
+++ src/utils/networker.cpp
@@ -7,5 +7,17 @@ Networker::Networker()
 
 QString Networker::requestData(const QUrl &url)
 {
-
+    QString faceitReply;
+    manager = new QNetworkAccessManager();
+    QEventLoop loop;
+    connect(manager, &QNetworkAccessManager::finished, &loop, &QEventLoop::quit);
+    request.setUrl(url);
+    qDebug() << "here";
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    request.setRawHeader("Authorization",  APIkey.toLocal8Bit());
+    QNetworkReply *reply = manager->get(request);
+    loop.exec();
+    qDebug() << reply->readAll();
+    faceitReply = *reply->readAll();
+    return faceitReply;
 }
--- src/utils/networker.h
+++ src/utils/networker.h
@@ -6,12 +6,21 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QNetworkRequest>
+#include <QDebug>
+#include <QObject>
+#include <QEventLoop>
 
-class Networker
+class Networker : public QObject
 {
 public:
     Networker();
     QString requestData(const QUrl &url);
+private:
+    QNetworkAccessManager *manager;
+    QNetworkRequest request;
+
+    QString token ="f4bd0606-7ff9-4925-aabf-82fc21c819ef";
+    QString APIkey = "Bearer " + token;
 };
 
 #endif // NETWORKER_H
