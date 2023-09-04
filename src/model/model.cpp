7eca288b4ca99347a10226b746cd5cdbd4968de6
Victor Zhou <victor.zhoujiang@tuni.fi>
2023-09-05 00:37:38 +0300

    started on model

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -1,6 +1,48 @@
 #include "model.h"
 
+QMap<team, QMap<map, QVector<winRates>>> mapWinRates;
+
 Model::Model()
 {
 
+}
+
+int Model::getElo(const QString &playerName)
+{
+    UserParser userInfo = getUserParserObject(playerName);
+    return userInfo.getFaceitElo();
+
+}
+
+int Model::getSkilLevel(const QString &playerName)
+{
+    UserParser userInfo = getUserParserObject(playerName);
+    return userInfo.getSkillLevel();
+}
+
+QMap<QString, QString> Model::getMapWinRates(QString &matchUrl)
+{
+    matchUrlBreaker UrlBreaker(matchUrl);
+    QString matchId = UrlBreaker.getMatchId();
+    UrlCreator urlCreator;
+    Networker networker;
+
+    QUrl url = urlCreator.createMatchUrl(matchId);
+    QString json = networker.requestData(url);
+
+    MatchParser matchParser;
+    QMap<QString, QVector<QString>> teams = matchParser.getData(json);
+
+
+}
+
+UserParser Model::getUserParserObject(const QString &playerName)
+{
+    UrlCreator urlCreator;
+    Networker networker;
+
+    QUrl url = urlCreator.createUserUrl(playerName);
+    QString json = networker.requestData(url);
+    UserParser userInfo(json);
+    return userInfo;
 }
--- src/model/model.h
+++ src/model/model.h
@@ -1,11 +1,27 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include "../utils/matchurlbreaker.h"
+#include "../utils/networker.h"
+#include "../utils/urlcreator.h"
+#include "../parsers/matchparser.h"
+#include "../parsers/userparser.h"
+#include "../parsers/userstatsparser.h""
+
+#include <QMap>
+#include <QString>
+#include <QVector>
 
 class Model
 {
 public:
     Model();
+
+private:
+    int getElo(const QString &playerName);
+    int getSkilLevel(const QString &playerName);
+    QMap<QString, QString> getMapWinRates(QString &matchUrl);
+    UserParser getUserParserObject(const QString &playerName);
 };
 
 #endif // MODEL_H
